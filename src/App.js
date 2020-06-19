2de7f82ca1a44245f7e120fa513c7ad32c5b6499
vasu <vasubabupotnuru@gmail.com>
2020-06-19 22:22:53 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,131 @@
 import React from 'react';
-import logo from './logo.svg';
+import RangePicker from "react-range-picker";
+// import 'date-fns';
+// import { DateRangePicker } from 'react-date-range';
+// import 'react-date-range/dist/styles.css'; // main style file
+// import 'react-date-range/dist/theme/default.css'; // theme css file
+// import Grid from '@material-ui/core/Grid';
+// import DateFnsUtils from '@date-io/date-fns';
+// import Checkbox from '@material-ui/core/Checkbox';
+// import {
+//     MuiPickersUtilsProvider,
+//     KeyboardTimePicker,
+//     KeyboardDatePicker,
+// } from '@material-ui/pickers';
+// import Radio from '@material-ui/core/Radio';
+// import RadioGroup from '@material-ui/core/RadioGroup';
+// import FormControlLabel from '@material-ui/core/FormControlLabel';
+// import FormControl from '@material-ui/core/FormControl';
+// import FormLabel from '@material-ui/core/FormLabel';
+
+
 import './App.css';
+// import {DatePicker} from "react-datepicker";
+
+
 
 function App() {
+
+
+    //
+    // const [selectedDate, setSelectedDate] = React.useState(new Date('2014-08-18T21:11:54'));
+    //
+    // const handleDateChange = (date) => {
+    //     setSelectedDate(date);
+    // };
+    // const [checked, setChecked] = React.useState(true);
+    //
+    // const handleChange = (event) => {
+    //     setChecked(event.target.checked);
+    // };
+    // const [value, setValue] = React.useState('female');
+    //
+    // const radioHandleChange = (event) => {
+    //     setValue(event.target.value);
+    // };
+    //
+    // const selectionRange = {
+    //     startDate: new Date(),
+    //     endDate: new Date(),
+    //     key: 'selection',
+    // }
+
+        //
+        // const [startDate, setStartDate] = React.useState(new Date());
+        // const [endDate, setEndDate] = React.useState(new Date());
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      {/*<DatePicker*/}
+  {/*    selected={startDate}*/}
+  {/*    onChange={date => setStartDate(date)}*/}
+  {/*    selectsStart*/}
+  {/*    startDate={startDate}*/}
+  {/*    endDate={endDate}*/}
+  {/*/>*/}
+  {/*<DatePicker*/}
+  {/*    selected={endDate}*/}
+  {/*    onChange={date => setEndDate(date)}*/}
+  {/*    selectsEnd*/}
+  {/*    startDate={startDate}*/}
+  {/*    endDate={endDate}*/}
+  {/*    minDate={startDate}*/}
+  {/*/>*/}
+
+
+  // <MuiPickersUtilsProvider utils={DateFnsUtils}>
+  //     {/*<Grid container justify="space-around">*/}
+  //     {/*    <KeyboardDatePicker*/}
+  //     {/*        disableToolbar*/}
+  //     {/*        variant="inline"*/}
+  //     {/*        format="MM/dd/yyyy"*/}
+  //     {/*        margin="normal"*/}
+  //     {/*        id="date-picker-inline"*/}
+  //     {/*        label="Date picker inline"*/}
+  //     {/*        value={selectedDate}*/}
+  //     {/*        onChange={handleDateChange}*/}
+  //     {/*        KeyboardButtonProps={{*/}
+  //     {/*            'aria-label': 'change date',*/}
+  //     {/*        }}*/}
+  //     {/*    />*/}
+  //     {/*    <KeyboardDatePicker*/}
+  //     {/*        disableToolbar*/}
+  //     {/*        variant="inline"*/}
+  //     {/*        format="MM/dd/yyyy"*/}
+  //     {/*        margin="normal"*/}
+  //     {/*        id="date-picker-inline"*/}
+  //     {/*        label="Date picker inline"*/}
+  //     {/*        value={selectedDate}*/}
+  //     {/*        onChange={handleDateChange}*/}
+  //     {/*        KeyboardButtonProps={{*/}
+  //     {/*            'aria-label': 'change date',*/}
+  //     {/*        }}*/}
+  //     {/*    />*/}
+  //     {/*</Grid>*/}
+  //     {/*<Grid container justify="space-around">*/}
+  //     {/*    Activated:<Checkbox*/}
+  //     {/*        defaultChecked*/}
+  //     {/*        color="primary"*/}
+  //     {/*        inputProps={{ 'aria-label': 'secondary checkbox' }}*/}
+  //     {/*    />*/}
+  //     {/*    Not Activated:<Checkbox*/}
+  //     {/*    defaultChecked*/}
+  //     {/*    color="primary"*/}
+  //     {/*    inputProps={{ 'aria-label': 'secondary checkbox' }}*/}
+  //     {/*/>*/}
+  //     {/*</Grid>*/}
+  //     {/*<FormControl component="fieldset" >*/}
+  //     {/*    <FormLabel component="legend">Gender</FormLabel>*/}
+  //     {/*    <RadioGroup aria-label="gender" name="gender1" value={value} onChange={radioHandleChange}>*/}
+  //     {/*        <FormControlLabel value="female" control={<Radio />} label="Female" />*/}
+  //     {/*        <FormControlLabel value="male" control={<Radio />} label="Male" />*/}
+  //     {/*        <FormControlLabel value="other" control={<Radio />} label="Other" />*/}
+  //     {/*        <FormControlLabel value="disabled" disabled control={<Radio />} label="(Disabled option)" />*/}
+  //     {/*    </RadioGroup>*/}
+  //     {/*</FormControl>*/}
+  // </MuiPickersUtilsProvider>
+  //
+
+
   );
 }
 
